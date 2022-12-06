07315e0ae1a3db210ef1be50db85154dd372491f
Marihuana-Kox <yakuhs@yandex.ru>
2022-12-06 22:34:17 +0300

    jquery add

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,10 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
-
 document.addEventListener('DOMContentLoaded', () => {
-
-
+    
+})
+$(document).ready(function() {
+    $('.chart-number').each(function(i) {
+        console.log($(this).text())
+    })
 })
